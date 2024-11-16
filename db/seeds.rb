1aaf03802b3a1048506a995032ef11f8fa5ee1d6
DÃelia Knoepfli <delia.knoepfli@gmail.com>
2024-11-16 13:51:03 +0100

    some fixes

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,17 +9,35 @@
 #   end
 
 
-require 'faker'
+# require 'faker'
 
-Movie.destroy_all
+
 
 puts 'Creating films'
 
-10.times do
-   Movie.create!(
-    title: Faker::Movie.title,
-    overview: Faker::Lorem.sentence(word_count: 3, supplemental: true, random_words_to_add: 15),
-    poster_url: Faker::Avatar.image(size: "300x300", format: "png"),
-    rating: rand(0..10)
-    )
+# 10.times do
+#    Movie.create!(
+#     title: Faker::Movie.title,
+#     overview: Faker::Lorem.sentence(word_count: 3, supplemental: true, random_words_to_add: 15),
+#     poster_url: Faker::Avatar.image(size: "300x300", format: "png"),
+#     rating: rand(0..10)
+#     )
+# end
+
+require 'uri'
+require 'net/http'
+
+url = URI.parse("https://tmdb.lewagon.com/movie/top_rated")
+response = Net::HTTP.get(url)
+movies = JSON.parse(response)["results"].first(10)
+
+
+movies.each do |movie|
+  Movie.create!(
+   title: movie["title"],
+   overview: movie["overview"],
+   poster_url: "https://image.tmdb.org/t/p/w500#{movie['poster_path']}",
+   rating: movie["vote_average"]
+   )
 end
+puts "created #{Movie.count} movies"
